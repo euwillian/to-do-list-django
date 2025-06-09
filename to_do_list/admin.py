e0fa2af30abc229bbeca2ad250cfb7ae56616a4e
Willian Santos <12131283+euwillian@users.noreply.github.com>
2025-06-09 18:45:19 -0300

    Ajustes

--- to_do_list/admin.py
+++ to_do_list/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from to_do_list.models import ToDoList
 
-# Register your models here.
+class TodoListAdmin(admin.ModelAdmin):
+    ...
+    # list_display
+    # search_display
+    # Nesta classe eu informo o que será apresentando e o que posso filtrar na url: /admin
+    
+    """
+    O arquivo admin.py serve para registrar seus modelos no painel administrativo do Django (o Django Admin), que é uma interface automática e super útil para criar, visualizar, editar e excluir dados dos modelos do seu projeto — sem precisar escrever código para isso.
+    
+    """
+
+
+admin.site.register(ToDoList)
