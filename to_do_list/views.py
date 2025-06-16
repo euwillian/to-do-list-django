da9b2d3ea983cc46ae9a511c00c11a10c579f7a0
Willian Santos <12131283+euwillian@users.noreply.github.com>
2025-06-16 19:20:16 -0300

    Crud completed

--- to_do_list/views.py
+++ to_do_list/views.py
@@ -1,22 +1,38 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, get_object_or_404
 from .models import ToDoList 
 
 # Render 
 
-def gravar_e_listar_tarefa(request):
+def crud_tarefa(request):
+    tarefa_edicao = None
+    
     # POST -> Envio de dados
     # GET  -> Recebimento de dados
-    
     if request.method == 'POST':
+        id = request.POST.get("id")
         descricao = request.POST.get("descricao")
+        acao = request.POST.get("acao")  # pode ser None, 'excluir', etc.
         
-        # cria uma nova instancia do modelo "ToDoList - Model" , e salva no banco de dados
-        
-        nova_tarefa = ToDoList(descricao=descricao)
-        nova_tarefa.save()
-        
-        # este redirect é importante para evitar o usuário pressionar F5 e salve novamente
+        if acao == 'excluir':
+            tarefa = get_object_or_404(ToDoList, id=id)
+            tarefa.delete()
+
+        elif id:  # edição
+            tarefa = get_object_or_404(ToDoList, id=id)
+            tarefa.descricao = descricao
+            tarefa.save()
+
+        else:  # criação
+            nova_tarefa = ToDoList(descricao=descricao)
+            nova_tarefa.save()
+
+        # este redirect é importante para evitar o usuário pressionar F5 e salvar novamente
         return redirect('todo')
+        
+    # Verifica se veio um ?editar=ID na URL (entrar no modo Edicao)
+    if 'editar' in request.GET:
+        tarefa_edicao = get_object_or_404(ToDoList, id=request.GET.get('editar'))
+    
     
     ###########################################
     # Listar todas tarefas do banco de dados  #
@@ -39,15 +55,7 @@ def gravar_e_listar_tarefa(request):
         'tarefas': tarefas,
         'tarefas_completas': tarefas_completas,
         'tarefas_incompletas': tarefas_incompletas,
+        'tarefa_edicao': tarefa_edicao,
     }
     
     return render(request, 'to_do.html', contexto)
-    
-
-def editar_tarefa(request):
-    ...
-    
-
-def excluir_tarefa(request):
-    ...
-    
